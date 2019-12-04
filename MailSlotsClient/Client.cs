83f13851bdfe6adbb87176f0dcc12ee601c68913
Kokovin Aleksei <rycarok@gmail.com>
2019-12-04 15:36:42 +0000

    Add project files.

---
+++ MailSlotsClient/Client.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace MailSlots
+{
+    public partial class frmMain : Form
+    {
+        private Int32 HandleMailSlot;                   // дескриптор мэйлслота
+        private Int32 HandleClientMailSlot;             // дескриптор приёмного мэйлслота
+        private Thread t;
+        private bool _continue = true;
+        // конструктор формы
+        public frmMain()
+        {
+            InitializeComponent();
+            this.Text += "     " + Dns.GetHostName();   // выводим имя текущей машины в заголовок формы
+            HandleClientMailSlot = DIS.Import.CreateMailslot("\\\\.\\mailslot\\ClientMailslot", 0, DIS.Types.MAILSLOT_WAIT_FOREVER, 0);
+            t = new Thread(ReceiveMessage);
+            t.Start();
+        }
+
+        private void ReceiveMessage()
+        {
+            string msg = "";            // прочитанное сообщение
+            int MailslotSize = 0;       // максимальный размер сообщения
+            int lpNextSize = 0;         // размер следующего сообщения
+            int MessageCount = 0;       // количество сообщений в мэйлслоте
+            uint realBytesReaded = 0;   // количество реально прочитанных из мэйлслота байтов
+
+            // входим в бесконечный цикл работы с мэйлслотом
+            while (_continue)
+            {
+                DIS.Import.GetMailslotInfo(HandleClientMailSlot, MailslotSize, ref lpNextSize, ref MessageCount, 0);
+
+                // если есть сообщения в мэйлслоте, то обрабатываем каждое из них
+                if (MessageCount > 0)
+                {
+                    for (int i = 0; i < MessageCount; i++)
+                    {
+                        byte[] buff = new byte[1024];
+                        DIS.Import.FlushFileBuffers(HandleClientMailSlot);      // "принудительная" запись данных, расположенные в буфере операционной системы, в файл мэйлслота
+                        DIS.Import.ReadFile(HandleClientMailSlot, buff, 1024, ref realBytesReaded, 0);      // считываем последовательность байтов из мэйлслота в буфер buff
+                        msg = Encoding.Unicode.GetString(buff);                 // выполняем преобразование байтов в последовательность символов
+                        int lastIdx = msg.IndexOf('\0');
+                        if (lastIdx != -1)
+                            msg = msg.Substring(0, lastIdx);
+                        tb_messages.Invoke((MethodInvoker)delegate
+                        {
+                            if (msg != "")
+                                tb_messages.Text += "\n >> " + msg + " \n";     // выводим полученное сообщение на форму
+                        });
+                    }
+                
+                }
+
+
+            }
+        }
+
+        // присоединение к мэйлслоту
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // открываем мэйлслот, имя которого указано в поле tbMailSlot
+                HandleMailSlot = DIS.Import.CreateFile(tbMailSlot.Text, DIS.Types.EFileAccess.GenericWrite, DIS.Types.EFileShare.Read, 0, DIS.Types.ECreationDisposition.OpenExisting, 0, 0);
+                if (HandleMailSlot != -1)
+                {
+                    send(Dns.GetHostName().ToString() + " << " + nameTb.Text);
+                    btnConnect.Enabled = false;
+                    btnSend.Enabled = true;
+                }
+                else
+                    MessageBox.Show("Не удалось подключиться к мейлслоту");
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось подключиться к мейлслоту");
+            }
+        }
+
+
+
+        private void send(string text)
+        {
+            uint BytesWritten = 0;  // количество реально записанных в мэйлслот байт
+            byte[] buff = Encoding.Unicode.GetBytes(text);    // выполняем преобразование сообщения (вместе с идентификатором машины) в последовательность байт
+
+            DIS.Import.WriteFile(HandleMailSlot, buff, Convert.ToUInt32(buff.Length), ref BytesWritten, 0);     // выполняем запись последовательности байт в мэйлслот
+        }
+
+        // отправка сообщения
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            send(Dns.GetHostName().ToString() + " >> " + tbMessage.Text);
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DIS.Import.CloseHandle(HandleMailSlot);     // закрываем дескриптор мэйлслота
+            _continue = false;
+            if (t != null)
+                t.Abort();          // завершаем поток
+            if (HandleClientMailSlot != -1)
+                DIS.Import.CloseHandle(HandleClientMailSlot);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MailSlotsClient/Client.designer.cs
@@ -0,0 +1,158 @@
+﻿namespace MailSlots
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.lblMailSlot = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.tbMailSlot = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTb = new System.Windows.Forms.TextBox();
+            this.tb_messages = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(274, 99);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(91, 26);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(86, 103);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(181, 20);
+            this.tbMessage.TabIndex = 1;
+            // 
+            // lblMailSlot
+            // 
+            this.lblMailSlot.AutoSize = true;
+            this.lblMailSlot.Location = new System.Drawing.Point(12, 15);
+            this.lblMailSlot.Name = "lblMailSlot";
+            this.lblMailSlot.Size = new System.Drawing.Size(75, 26);
+            this.lblMailSlot.TabIndex = 2;
+            this.lblMailSlot.Text = "Введите имя \r\nмейлслота";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(274, 6);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(91, 76);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Подключиться";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(7, 106);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(65, 13);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Сообщение";
+            // 
+            // tbMailSlot
+            // 
+            this.tbMailSlot.Location = new System.Drawing.Point(86, 12);
+            this.tbMailSlot.Name = "tbMailSlot";
+            this.tbMailSlot.Size = new System.Drawing.Size(181, 20);
+            this.tbMailSlot.TabIndex = 0;
+            this.tbMailSlot.Text = "\\\\.\\mailslot\\ServerMailslot";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите имя\r\nпользователя";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nameTb
+            // 
+            this.nameTb.Location = new System.Drawing.Point(86, 53);
+            this.nameTb.Name = "nameTb";
+            this.nameTb.Size = new System.Drawing.Size(181, 20);
+            this.nameTb.TabIndex = 0;
+            this.nameTb.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tb_messages
+            // 
+            this.tb_messages.Location = new System.Drawing.Point(10, 137);
+            this.tb_messages.Name = "tb_messages";
+            this.tb_messages.Size = new System.Drawing.Size(355, 166);
+            this.tb_messages.TabIndex = 4;
+            this.tb_messages.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 315);
+            this.Controls.Add(this.tb_messages);
+            this.Controls.Add(this.nameTb);
+            this.Controls.Add(this.tbMailSlot);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblMailSlot);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Клиент";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Label lblMailSlot;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox tbMailSlot;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTb;
+        private System.Windows.Forms.RichTextBox tb_messages;
+    }
+}
+
